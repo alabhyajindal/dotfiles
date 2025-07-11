fb32842feda244b7d27a731ad8df172252e29617
Alabhya Jindal <aj@alabhya.me>
2025-07-11 01:39:52 +0100

    Update

--- fish/.config/fish/functions/wasmc.fish
+++ fish/.config/fish/functions/wasmc.fish
@@ -1,3 +1,3 @@
 function wasmc
-    /opt/wasi-sdk-25.0-x86_64-linux/bin/clang --sysroot=/opt/wasi-sdk-25.0-x86_64-linux/share/wasi-sysroot/ $argv
+    /opt/wasi-sdk/bin/clang $argv -o (string replace c wasm $argv[1])
 end
